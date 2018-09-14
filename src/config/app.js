ef8f261d6ba8b49609f7e952de630ae9a844a77b
Lucas Wilker <lucas.barbosa@ccc.ufcg.edu.br>
2018-09-13 22:09:15 -0300

    Setup init

---
+++ src/config/app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var router = express.Router();
+
+// var mongoose = require('mongoose');
+//conexao BD
+// mongoose.connect('mongodb://localhost:27017/2vdb');
+// const user = require('./models/User');
+//router
+// var indexRouter = require('./routes/index');
+
+const route = router.get('/', (req, res) => {
+    res.send({
+        message: 'DEU CERTO, PORRA'
+    })
+});
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.use('/', route);
+
+module.exports = app;
+
+
+// app.set('view engine', 'ejs');
+// view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.use('/users', usersRouter);
---
+++ src/config/mongoose.js
@@ -0,0 +1,20 @@
+let mongoose = require("mongoose");
+
+module.exports = function () {
+    mongoose.connect("mongodb://localhost:27017/gestao-capacitacao");
+    mongoose.Promise = global.Promise;
+
+    let db = mongoose.connection;
+
+    db.on("connected", function() {
+        console.log("conectado");
+    });
+
+    db.on("disconnected", function() {
+        console.log("desconectado");
+    });
+
+    db.on("error", function(error) {
+        console.log('Erro na conexão: ' + error);
+    });
+};
