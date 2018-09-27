af781f3ed9c837d7d76401e31430721c14df0f9a
Lucas Wilker <lucas.barbosa@ccc.ufcg.edu.br>
2018-09-27 17:36:04 -0300

    Created swagger docs

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,9 +1,14 @@
 const userRouter = require('../user/user.routes');
 const thingRouter = require('./../things/thing.routes');
 const adminRouter = require('./../admin/admin.routes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./../config/swagger-docs/swagger.json');
 
 module.exports = (app) => {
     app.use('/api/2V/user', userRouter);
     app.use('/api/2V/thing', thingRouter);
     app.use('/api/2V/admin', adminRouter);
+
+    app.use('/api-docs-swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 }
--- src/user/user.routes.js
+++ src/user/user.routes.js
@@ -4,14 +4,14 @@ const userController = require('./user.controller');
 
 //rotas de usuarios
 router.post('/', userController.post);
+ 
+router.get('/:id', userController.getById); 
 
-router.get('/:id', userController.getById);
+router.get('/:userId/itens', userController.getItens); 
 
-router.get('/:userId/itens', userController.getItens);
+router.post('/:id', userController.addItem); 
 
-router.post('/:id', userController.addItem);
-
-router.put('/:userId/item/:itemId', userController.returnedItem);
+router.put('/:userId/item/:itemId', userController.returnedItem); 
 
 router.get('/:userId/item/:itemId', userController.solicitedItem); //quando clicar no botão C -FRONT
 
