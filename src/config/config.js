0566b91968d4738f09106cc9025bb132a18936d5
Lucas Wilker <lucas.barbosa@ccc.ufcg.edu.br>
2018-10-09 09:27:23 -0300

    Update var mongod heroku

--- src/config/config.js
+++ src/config/config.js
@@ -6,7 +6,7 @@ var systemHour  = {
 } 
  
 module.exports = { 
-    connectionDBHeroku: "mongodb://2v-database:wilker225414@ds131902.mlab.com:31902/2v-database",
+    connectionDBHeroku: "mongodb://2vdb:wilker225414@ds131902.mlab.com:31902/2v-database",
     connectionDB: "mongodb://localhost:27017/2vdb", 
     sendGridKey: "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", 
     email: "2VService@email.com", 
