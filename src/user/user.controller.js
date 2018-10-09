37ff6965a3876113f9291d2874ec687ac1ab4fea
Lucas Wilker <lucas.barbosa@ccc.ufcg.edu.br>
2018-10-09 11:33:39 -0300

    Return res status code

--- src/user/user.controller.js
+++ src/user/user.controller.js
@@ -118,7 +118,7 @@ exports.removeItem = ('/remove-item', async(req, res) => {
     var token = req.body.token || req.query.token || req.headers['x-access-token'];
     const data = await auth.decodeToken(token);
     
-    if(req.params.userId != data.id) return res.send(response.unauthorized('Acesso não autorizado!'));
+    if(req.params.userId != data.id) return res.status(401).send(response.unauthorized('Acesso não autorizado!'));
     
     try {
         await userService.removeItemInReturned(req.params.userId, req.params.itemId, (response) => {
